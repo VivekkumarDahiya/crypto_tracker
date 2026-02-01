0bcd7a29133f236745efd7af302345f235851cc3
Vivek Kumar Dahiya <dahiyavivek933@gmail.com>
2026-02-01 09:26:20 +0530

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
 
 function App() {
  
 
   return (
     <>
-    <h1>hello world</h1>
+    <Header/>
+    <Footer/>
     </>
   )
 }
---
+++ src/components/common/Header/index.jsx
@@ -0,0 +1,13 @@
+import style from "./header.module.css";
+
+
+
+const Header=()=>{
+    return(
+        <header  className={style.header}>
+             Header
+        </header>
+    )
+}
+
+export default Header;
